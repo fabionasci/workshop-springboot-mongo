44d12b4319634e85afb0732f62e07c9d265b94d6
Fabio Nascimento <nascimento.fab@gmail.com>
2025-12-11 00:12:37 -0300

    Consulta simples com @Query

--- src/main/java/com/curso/java/completo/workshop_mongo/repository/PostRepository.java
+++ src/main/java/com/curso/java/completo/workshop_mongo/repository/PostRepository.java
@@ -2,11 +2,16 @@ package com.curso.java.completo.workshop_mongo.repository;
 
 import com.curso.java.completo.workshop_mongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-    List<Post> findByTitleContaining(String title);
+
+    @Query("{'title': { $regex: ?0, $options: 'i'}}")
+    List<Post> searchTitle(String title);
+
+    List<Post> findByTitleContainingIgnoreCase(String title);
 }
